f7d7a999325fd7e319d4f7fc75beba6f01071297
fortydeux <ogtpowell@gmail.com>
2024-02-20 14:23:01 -0500

    Addoed config line to ensure sudo is enabled on system

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -34,6 +34,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Sudo
+  security.sudo.enable = true;
+
   networking.hostName = "blackfin-nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
