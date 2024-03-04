204424f285b6f36205ebaf97a05a9183ded86403
Fortydeux <ogtpowell@gmail.com>
2024-03-04 09:24:24 -0500

    Flake updated and built to confirm.

--- home-manager/dotfiles/nvim/init.lua
+++ home-manager/dotfiles/nvim/init.lua
@@ -230,6 +230,21 @@ require('lazy').setup {
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
+  --
+"brenton-leighton/multiple-cursors.nvim",
+version = "*",  -- Use the latest tagged version
+opts = {},  -- This causes the plugin setup function to be called
+keys = {
+  {"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i"}},
+  {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>"},
+  {"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i"}},
+  {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>"},
+  {"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}},
+  {"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}},
+  {"<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}},
+  {"<Leader>d", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}},
+  {"<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>"},
+},
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
