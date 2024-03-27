fc41085163be6d140cc0478ca2714b0ca7600c13
Fortydeux <ogtpowell@gmail.com>
2024-03-27 10:04:43 -0400

    Flake updated and MuseScore music transcription app pkg added to home-commonConfig.nix

--- home-manager/home-modules/home-commonConfig.nix
+++ home-manager/home-modules/home-commonConfig.nix
@@ -23,24 +23,25 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # hello
-    gh #Github CLI tool 
-    discord #Discord social client
-    logseq #Logseq electron desktop client
-    reaper #Reaper DAW
-    signal-desktop #Signal electron desktop client
-    telegram-desktop #Telegram desktop client
-    simplex-chat-desktop #SimpleX Chat Desktop Client
-    media-downloader #Media-downloader desktop client
-    libsForQt5.kdenlive #KdenLive Video Editor 
     anytype #P2P note-taking tool
     appflowy #An open-source alternative to Notion
-    lan-mouse #Wayland software KVM switch
-    mediawriter #USB imaage writer
-    ticktick #A powerful to-do & task management app with seamless cloud synchronization across all your devices
-    obs-studio #Screen recorder
-    spotify #Spotify music client - Requires non-free packages enabled
-    yt-dlp #Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
+    discord #Discord social client
+    gh #Github CLI tool 
     joplin-desktop #An open source note taking and to-do application with synchronisation capabilities
+    logseq #Logseq electron desktop client
+    libsForQt5.kdenlive #KdenLive Video Editor 
+    lan-mouse #Wayland software KVM switch
+    media-downloader #Media-downloader desktop client
+    mediawriter #USB imaage writer
+    musescore #Music notation and composition software
+    obs-studio #Screen recorder
+    reaper #Reaper DAW
+    signal-desktop #Signal electron desktop client
+    simplex-chat-desktop #SimpleX Chat Desktop Client
+    spotify #Spotify music client - Requires non-free packages enabled
+    telegram-desktop #Telegram desktop client
+    ticktick #A powerful to-do & task management app with seamless cloud synchronization across all your devices
+    yt-dlp #Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
 #Not yet available on Linux through nixpkgs    warp-terminal # Modern rust-based terminal       
   ]);
     # # It is sometimes useful to fine-tune packages, for example, by applying
