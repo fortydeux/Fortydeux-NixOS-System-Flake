f841af9d8d027bc97bfbf0c29e53f5bb4962e9bf
fortydeux <ogtpowell@gmail.com>
2024-04-16 09:38:20 -0400

    CLI PDF Utils added to home config.

--- home-manager/home-modules/home-commonConfig.nix
+++ home-manager/home-modules/home-commonConfig.nix
@@ -42,14 +42,15 @@ imports = [
     media-downloader #Media-downloader desktop client
     mediawriter #USB imaage writer
     musescore #Music notation and composition software
-    obs-studio #Screen recorder
+    obs-studio #Screen recorder       
+    poppler_utils #Poppler is a PDF rendering library based on the xpdf-3.0 code base. In addition it provides a number of tools that can be installed separately.    
     reaper #Reaper DAW
     signal-desktop #Signal electron desktop client
     simplex-chat-desktop #SimpleX Chat Desktop Client
     spotify #Spotify music client - Requires non-free packages enabled
     telegram-desktop #Telegram desktop client
     ticktick #A powerful to-do & task management app with seamless cloud synchronization across all your devices
-    vscode 
+    vscode #Open source source code editor developed by Microsoft for Windows, Linux and macOS    
     yt-dlp #Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
     zoom-us #zoom.us video conferencing application
     warp-terminal # Modern rust-based terminal       
