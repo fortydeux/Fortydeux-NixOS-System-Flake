ce314422cbce2fd4e928e101c2d360e81f0e8d6b
fortydeux <ogtpowell@gmail.com>
2024-06-04 09:05:00 -0400

    Wireplumber service now added explicitly to commonConfig.nix; Super-Productivity added to home config.

--- nixos-config/system-modules/common-config.nix
+++ nixos-config/system-modules/common-config.nix
@@ -213,14 +213,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    wireplumber.enable = true;
+      # If you want to use JACK applications, uncomment this
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
+  
+  
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
