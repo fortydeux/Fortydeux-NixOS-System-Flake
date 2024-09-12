d5d93be54e76b592a958a7cb5b937c8fb9fa53d5
Fortydeux <ogtpowell@gmail.com>
2024-09-12 07:41:16 -0400

    Removes xserver and xinit from plasma.nix

--- nixos-config/system-modules/plasma.nix
+++ nixos-config/system-modules/plasma.nix
@@ -6,13 +6,9 @@
   services.desktopManager.plasma6.enable = true;
 
   # Enable Xorg session
-  services.xserver.enable = true;
-
-  # environment.plasma6.excludePackages = with pkgs.kdePackages; [
-  #   systemsettings
+  # services.xserver.enable = true;
+ 
+  # environment.systemPackages = with pkgs; [
+  #   xorg.xinit
   # ];
-  
-  environment.systemPackages = with pkgs; [
-    xorg.xinit
-  ];
 }
