236cfe91569ea6b4a8b2b20f392612eca8c03564
fortydeux <ogtpowell@gmail.com>
2024-09-27 12:36:12 -0400

    Nixifies hyprland config and disables home-files write of hyprland conf; now correctly loads enabled plugins in hyprland-config.nix - TODO: switch over device-specific hyprland config

--- home-manager/home-modules/hyprland-config.nix
+++ home-manager/home-modules/hyprland-config.nix
@@ -10,39 +10,279 @@
 	wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.default;
-    systemd.variables = ["--all"];
+    # systemd.variables = ["--all"];
     plugins = [
       # Hyprexpo plugin
       inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo   
       # pkgs.hyprlandPlugins.hyprexpo
       # "${pkgs.hyprexpo}/lib/libhyprexpo.so"       
       # Hyprgrass plugin
-      inputs.hyprgrass.packages.${pkgs.system}.hyprgrass
+      # inputs.hyprgrass.packages.${pkgs.system}.hyprgrass
       # "${hyprgrass}/lib/hyprgrass.so"       
       # Hyprscroller plugin
-      pkgs.hyprlandPlugins.hyprscroller       
+      # pkgs.hyprlandPlugins.hyprscroller       
       # "${hyprscroller}/lib/hyprscroller.so"       
       # Hyprscpace plugin
-      inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
+      # inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
       # "${hyprspace}/lib/hyprspace.so"       
       # Hyprland Virtual Desktops Plugin
-      inputs.hyprland-virtual-desktops.packages.${pkgs.system}.virtual-desktops
+      # inputs.hyprland-virtual-desktops.packages.${pkgs.system}.virtual-desktops
     ];
-    settings = {
-       plugin = {
-              hyprexpo = {
-                columns = 3;
-                gap_size = 5;
-                bg_col = "rgb(111111)";
-                workspace_method = "first current"; # [center/first] [workspace] e.g. first 1 or center m+1
-                enable_gesture = true; # laptop touchpad, 4 fingers
-                gesture_distance = 300; # how far is the "max"
-                gesture_positive = true; # positive = swipe down. Negative = swipe up.
-              };
-            };         
+    settings = { 
+      "$mainMod" = "SUPER";
+      exec-once = [
+        "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY"
+        "hyprctl setcursor phinger-cursors 32"
+        "emacs --daemon"
+        "foot -s"
+         "$waybar"
+        "mako"
+        "nm-applet --indicator"
+        "blueman-applet"
+        "pcloud"
+        "wlsunset -l 40.6 -L -75.4 -t 2300 -T 6500"
+        "$swaybg-x"
+        "$hypridle"
+        
+      ];
+      input = {
+          kb_layout = "us";
+
+          follow_mouse = 1;
+          natural_scroll = true;
+          touchpad = {
+              natural_scroll = true;
+              middle_button_emulation = false;
+              disable_while_typing = true;
+              clickfinger_behavior = true;
+              scroll_factor = 2;
+          };
+          sensitivity = 0.3;
+      };
+
+      general = {
+          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+          gaps_in = 4;
+          gaps_out = 4;
+          border_size = 2;
+          "col.active_border" =  "$activeBorderColor1 $activeBorderColor2 8deg";
+          "col.inactive_border" = "$inactiveBorderColor";
+          #allow_session_lock_restore = true
+          layout = "dwindle";
+      };
+
+      decoration = {
+          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+         rounding = 12;
+         blur = {
+              enabled = true;
+              size = 3;
+              passes = 1;
+          };
+
+          drop_shadow = true;
+          shadow_range = 4;
+          shadow_render_power = 3;
+          "col.shadow" = "rgba(1a1a1aed)";
+      };
+
+      animations = {
+          enabled = true;
+
+          # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+          bezier = [
+             "myBezier, 0.05, 0.9, 0.1, 1.05"
+
+             # Added from garden theme
+             "slow, 0, 0.85, 0.3, 1"
+             "overshot, 0.7, 0.6, 0.1, 1.1"
+             "bounce, 1, 1.6, 0.1, 0.85"
+             "slingshot, 1, -2, 0.9, 1.25"
+             "nice, 0, 6.9, 0.5, -4.20"
+           ];
+           #    animation = windows, 1, 7, myBezier
+           animation = [
+             "windows, 1, 5, bounce, slide"
+             "windowsOut, 1, 7, default, popin 80%"
+             "border, 1, 20, default"
+             "borderangle, 1, 8, default"
+             "fade, 1, 7, default"
+            #    animation = workspaces, 1, 6, default
+             "workspaces, 1, 5, overshot, slide"
+           ];
+      };
+
+      dwindle = {
+          # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+          pseudotile = true;
+          preserve_split = true;
+      };
+
+      master = {
+          # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+          new_status = "master";
+      };
+
+      gestures = {
+          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+          workspace_swipe = true;
+          workspace_swipe_fingers = 3;
+      };
+
+      # Example per-device config
+      # See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
+      device = {
+        name = "microsoft-arc-mouse";
+          sensitivity = 1;
+      };
+
+      misc = {
+          disable_hyprland_logo = true;
+          disable_splash_rendering = true;
+      };
+      bind = [
+        "$mainMod, S, exec, kitty"
+        "CTRL ALT, T, exec, wezterm"
+        "$mainMod, RETURN, exec, footclient"
+        "$mainMod SHIFT, T, exec, alacritty"
+        "$mainMod, Q, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, E, exec, nautilus"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, D, exec, rofi -show drun -show-icons"
+        "$mainMod, W, exec, wofi -S drun -GIm -w 3 -W 100% -H 96%"
+        "$mainMod, P, pseudo, "
+        "$mainMod, L, exec, $hyprlock"
+        "$mainMod, J, togglesplit, "
+        "$mainMod, F, fullscreen, 0"
+        "$mainMod, H, fullscreen, 1"
+        "CTRL, SPACE, exec, pkill fuzzel || $fuzzel"
+        "$mainMod, B, exec, pkill waybar || $waybar"
+        "$mainMod, K, exec, kate"
+        ", PRINT, exec, grim -g '$(slurp)'"
+        "$mainMod SHIFT, X, exec, $swaybg-x"
+        "$mainMod SHIFT, Z, exec, $mpvpaper-z"
+        "$mainMod SHIFT, C, exec, $mpvpaper-c"
+
+        # Volume
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
+        # Playerctl
+        ", XF86AudioPlay, exec, playerctl play-pause"
+       
+        #Switches
+        "SUPER, Escape, exec, hyprctl reload"        
+        "SUPER, Escape, exec, pkill waybar && sleep 1 && $waybar &"
+        "SUPER, Escape, exec, notify-send 'Config Reloaded'"
+
+        # Move focus with mainMod + arrow keys
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        # Switch workspaces with mainMod + [0-9]
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+        "$mainMod SHIFT CTRL, right, movetoworkspace, +1"
+        "$mainMod SHIFT CTRL, left, movetoworkspace, -1"
+
+        # Scroll through existing workspaces with mainMod + CTRL + right/left
+        "$mainMod + CTRL, right, workspace, e+1"
+        "$mainMod + CTRL, left, workspace, e-1"
+        ", mouse_right, workspace, e+1"
+        ", mouse_left, workspace, e-1"
+        "$mainModCTRL, mouse:273, workspace, m+1"
+        "$mainModCTRL, mouse:272, workspace, m-1"      
+
+        # Touchscreen Gestures
+        # swipe left from right edge
+        ", edge:r:l, workspace, +1"
+
+        # swipe up from bottom edge
+        ", edge:d:u, exec, firefox"
+
+        # swipe down from left edge
+        ", edge:l:d, exec, pactl set-sink-volume @DEFAULT_SINK@ -4%"
+
+        # swipe down with 4 fingers
+        ", swipe:4:d, killactive"
+
+        # swipe diagonally leftwards and downwards with 3 fingers
+        # l (or r) must come before d and u
+        ", swipe:3:ld, exec, foot"
+
+
+        # New and experimental - Hyprexpo        
+        "SUPER, grave, hyprexpo:expo, toggle # can be: toggle, off/disable or on/enable"
+      ];
+
+      binde = [
+        ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
+
+        # Screen brightness
+        ", XF86MonBrightnessDown, exec, brightnessctl set 2%-"
+        ", XF86MonBrightnessUp, exec, brightnessctl set 2%+"
+      ];
+
+      bindl = [
+        #Switches
+        ", switch:Lid Switch, exec, $hyprlock"           
+      ];
+
+      bindm = [       
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod + SHIFT, mouse:272, resizewindow"
+      ];
+      
+      plugin = {
+        hyprexpo = {
+          columns = 3;
+          gap_size = 5;
+          bg_col = "rgb(111111)";
+          workspace_method = "first current"; # [center/first] [workspace] e.g. first 1 or center m+1
+          enable_gesture = true; # laptop touchpad, 4 fingers
+          gesture_distance = 300; # how far is the "max"
+          gesture_positive = true; # positive = swipe down. Negative = swipe up.
+        };
+      };         
     };
 		extraConfig = ''
-      bind = SUPER, grave, hyprexpo:expo, toggle
+      $activeBorderColor1 = rgba(da0c81cc) 
+      $activeBorderColor2 = rgba(940b92cc) 
+      $inactiveBorderColor = rgba(00000099) 
+      $swaylock = swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2
+      $swaybg-x = pkill mpvpaper & swaybg -m fill -i $HOME/.config/hypr/Wallpapers/Professional/balloon-wp.jpg
+      $mpvpaper-z = pkill swaybg & mpvpaper -p --slideshow 60 -o 'no-audio shuffle --speed=0.3' eDP-1 $HOME/.config/hypr/Wallpapers/Fun/Garden/
+      $mpvpaper-c = pkill swaybg & mpvpaper -p --slideshow 60 -o 'no-audio shuffle --speed=0.3' eDP-1 $HOME/.config/hypr/Wallpapers/Fun/CyberNeon/
+      $waybar = waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css
+      $fuzzel = fuzzel -w 80 -b 181818ef -t ccccccff
+      $hypridle = hypridle
+      $hyprlock = hyprlock
+      # source = $HOME/.config/hypr/device-specific.conf
     '';
 	};
   # xdg.portal = {
