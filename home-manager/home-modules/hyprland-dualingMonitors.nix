fcd4335869afa27ddc9c7c07591e358d7aa60c63
fortydeux <ogtpowell@gmail.com>
2024-10-08 09:01:23 -0400

    Adds display rotation to hyprland-dualingMonitors.nix for Blacktetra machine vertical monitor

--- home-manager/home-modules/hyprland-dualingMonitors.nix
+++ home-manager/home-modules/hyprland-dualingMonitors.nix
@@ -4,8 +4,8 @@
 	wayland.windowManager.hyprland = {
 	    extraConfig = ''
 	    	# Dual Monitors Setup:
-	    	monitor=DP-1,preferred,0x0,auto
-	    	monitor=HDMI-A-1,preferred,3440x360,auto
+	    	monitor=DP-1,preferred,0x221,auto
+	    	monitor=HDMI-A-1,preferred,3440x0,auto,transform,3
 	    '';
 	};
 }
