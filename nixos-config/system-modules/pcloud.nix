60068697ccfe11105f57368af931d16981ffe1b7
fortydeux <ogtpowell@gmail.com>
2025-02-23 10:13:42 -0500

    Adds comments to make pcloud.nix versions more clear

--- nixos-config/system-modules/pcloud.nix
+++ nixos-config/system-modules/pcloud.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 
 let
+  # Patch to fix patchelf issues with nixpkgs version
   patchelfFixes = pkgs.patchelfUnstable.overrideAttrs (_finalAttrs: _previousAttrs: {
     src = pkgs.fetchFromGitHub {
       owner = "Patryk27";
@@ -9,15 +10,17 @@ let
       sha256 = "sha256-3I089F2kgGMidR4hntxz5CKzZh5xoiUwUsUwLFUEXqE=";
     };
   });
+  # Patched version
   pcloudFixes = pkgs.pcloud.overrideAttrs (_finalAttrs:previousAttrs: {
     nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ patchelfFixes ];
   });
+  # Unrelated to above - this calls pcloud-package.nix to build pcloud directly, rather than waiting for nixpkgs-unstable to update
   pcloud = pkgs.callPackage ./pcloud-package.nix {};
 in
 {
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = [
-    # pcloudFixes
-    pcloud
+    # pcloudFixes #Patched version
+    pcloud #pcloud-package.nix local build version
   ];
 }
