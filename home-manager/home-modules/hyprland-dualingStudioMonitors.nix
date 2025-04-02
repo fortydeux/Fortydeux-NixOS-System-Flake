628d9a59633afff5e86bb9558448bc61e6c075f9
fortydeux <ogtpowell@gmail.com>
2025-04-02 10:58:17 -0400

    Changes workspace assignment for hyprland-dualingStudioMonitors.nix so that HDMI-A-2 is now set as workspace 1; updates flake.

--- home-manager/home-modules/hyprland-dualingStudioMonitors.nix
+++ home-manager/home-modules/hyprland-dualingStudioMonitors.nix
@@ -7,6 +7,10 @@
 	    	monitor=HDMI-A-2,preferred,0x0,auto
 	    	monitor=HDMI-A-1,preferred,1920x0,auto
 	    	# monitor=HDMI-A-1,preferred,3440x0,auto,transform,3
+	    	
+	    	# Workspace assignments
+	    	workspace=1,monitor:HDMI-A-2
+	    	workspace=2,monitor:HDMI-A-1
 	    '';
 	};
 }
